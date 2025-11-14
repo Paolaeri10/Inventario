1755659ba816e3c86de92d5a98907bcc4c241770
Paolaeri10 <paoeri10@gmail.com>
2025-11-13 21:40:48 -0600

    Update prueba2.java

--- prueba2.java
+++ prueba2.java
@@ -9,16 +9,40 @@ public class void main(String[] args){
  Systema.out.println("\n \n \n");
 
 
- // Variables principales 
+ // variables principales 
  System.out.println(" ingresar la demanda anual (D): ");
  double D = lector.nextDouble();
  System.out.println(" ingrese el costo por ordenar (S): ");
  double S = lector.nextDouble();
  System.out.println("ingresa el costo de mantener el inventario por unidad (H): ");
  double H = lector.nextDouble();
+ System.out.print("Costo por unidad (C): ");
+ double C = lector.nextDouble();
  System.out.println("\n Hay descuento por cantidad (1 = si, 0 = No: ");
  int opcion = lector.nextInt();
+double descuento = 0;
 
- 
- 
+        if(d == 1){
+            System.out.print("Ingresa el % de descuento (ej 0.10): ");
+            descuento = lector.nextDouble();
+            C = C - (C * descuento); // aqui le bajo el precio
+            System.out.println("Nuevo costo unitario: "+C);
+        }
+
+        // fórmula EOQ
+        double Q = Math.sqrt((2 * D * S) / H);
+
+        // fórmula CT
+        double CT = (D/Q)*S + (Q/2)*H + (D*C);
+
+        // resultados
+        System.out.println("\n---- RESULTADOS ----");
+        System.out.println("EOQ o cantidad económica: " + Q);
+        System.out.println("Costo total anual CT: " + CT);
+
+        // comentario random de arte
+        System.out.println("Esto sirve para no quedarte sin pintura en tu taller y sin material :)");
+    }
 }
+
+
